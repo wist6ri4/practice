4ee6606b99654f0c849fb0a310c475a68d24ffd7
Shiro Goto <sg1106.footballdrummer.51046@gmail.com>
2024-11-29 00:25:40 +0900

    20241129 CheckGCの作成

---
+++ 20241119_CheckGC/test.js
@@ -0,0 +1,31 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const profile = {
+    name: "soarflat",
+    sex: "male",
+    location: "Tokyo",
+};
+
+const hoge = (message) => {
+    console.log(message);
+};
+
+// hoge(new Person('Person').name);
+
+const fooBar = (a, b, c) => {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+};
+
+fooBar(
+    111,
+    {
+        hoge: "hoge!",
+    },
+    profile
+);
